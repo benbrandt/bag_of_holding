52f6b75039fd2c7a008895689aba66a1bafcc9f3
Ben Brandt <benjamin.j.brandt@gmail.com>
2022-03-17 22:02:10 +0100

    Check if average of rolls is within given range

--- crates/dice/tests/integration_test.rs
+++ crates/dice/tests/integration_test.rs
@@ -1,14 +1,30 @@
 use dice::Die;
+use itertools::Itertools;
 use rand::SeedableRng;
 use rand_pcg::Pcg64;
+use statrs::{
+    distribution::ChiSquared,
+    statistics::{Distribution, Statistics},
+};
 use strum::IntoEnumIterator;
 
 #[test]
 fn roll() {
     let mut rng = Pcg64::from_entropy();
+
     for die in Die::iter() {
-        let roll = die.roll(&mut rng);
-        assert!((1..=die.into()).contains(&roll));
+        let die_num: u32 = die.into();
+        let die_avg = (die_num - 1) as f64 / 2.0;
+        let std_dev = ChiSquared::new(die_avg).unwrap().std_dev().unwrap();
+
+        let rolls = (0..100)
+            .into_iter()
+            .map(|_| die.roll(&mut rng))
+            .collect_vec();
+
+        assert!(rolls.iter().all(|roll| (1..=die_num).contains(roll)));
+        let mean = rolls.iter().map(|&r| r as f64).mean();
+        assert!((mean - die_avg).abs() < std_dev);
     }
 }
 
