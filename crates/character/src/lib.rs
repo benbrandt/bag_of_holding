c311fdb413166f4dd7bf4c0ea46430c4a3ce6b10
Ben Brandt <benjamin.j.brandt@gmail.com>
2022-07-19 21:24:42 +0200

    (character) initial commit

---
+++ crates/character/src/lib.rs
@@ -0,0 +1,24 @@
+//! # Characters
+//!
+//! Crate to generate entire characters. Assembles together all of the other
+//! crates together into a final character sheet.
+#![warn(
+    clippy::pedantic,
+    future_incompatible,
+    missing_debug_implementations,
+    missing_docs,
+    nonstandard_style,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    unused
+)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
---
+++ crates/character/tests/integration_test.rs
@@ -0,0 +1,14 @@
+#![warn(
+    clippy::pedantic,
+    future_incompatible,
+    missing_debug_implementations,
+    missing_docs,
+    nonstandard_style,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    unused
+)]
+
+#[test]
+fn generate_character() {}
