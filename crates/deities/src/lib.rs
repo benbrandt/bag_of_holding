5c2245052102247b9c3ffd52658e66d7b8a438c7
Ben Brandt <benjamin.j.brandt@gmail.com>
2022-10-30 08:28:56 +0100

    (deities) less weight for forgotten realms

--- crates/deities/src/lib.rs
+++ crates/deities/src/lib.rs
@@ -381,7 +381,7 @@ impl Pantheon {
     fn weight(self) -> i32 {
         match self {
             // Default
-            Self::ForgottenRealms => 4,
+            Self::ForgottenRealms => 2,
             // Racial
             Self::Bugbear
             | Self::Dragon
