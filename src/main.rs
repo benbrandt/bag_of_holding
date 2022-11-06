809cee15a8f1e161945964cb4144979085a6a85a
Ben Brandt <benjamin.j.brandt@gmail.com>
2022-11-06 13:25:09 +0100

    Update sentry

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,8 @@ async fn main() {
     let _guard = sentry::init(ClientOptions {
         release: release_name!(),
         traces_sample_rate: 0.1,
+        enable_profiling: true,
+        profiles_sample_rate: 0.1,
         ..ClientOptions::default()
     });
 
